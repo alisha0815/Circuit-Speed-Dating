d99e84032cce4fdb85b2e1084039d364bcdb1d45
jungah sohn <aliciasohn815@gmail.com>
2021-06-02 19:10:29 +0200

    data seeding unfinished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning databse..'
+
+User.destroy_all
+
+p 'Destroying users..'
+
+puts 'Creating users'
+
+photo_one = URI.open('https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60')
+
+user_one = User.new(
+    email: Faker::Internet.email(domain: 'gmail'),
+    password: "password",
+    name: "Jen",
+    description: "About Me👩: I like to sing-talk, guys with messy hair, reading the New Yorker on Sunday mornings, and funny voices. About You🧑: If you are still reading, that's pretty good Let's do this.",
+    age: 25,
+    gender: "Female",
+    height: "Student",
+    photo: "I play the flute"
+    )
+user_one.photo.attach(io: photo_one, filename: 'temp.png', content_type: 'image/png')
+user_one.save!
+puts "created!"
