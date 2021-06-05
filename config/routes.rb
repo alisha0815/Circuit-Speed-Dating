03ed5f9590112c153dc9722e144761efcaea2b93
alin epifanov <alin.epifanov@yahoo.com>
2021-06-05 17:56:19 +0200

    Users profiles pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  # get 'profile', to: 'profiles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :profiles, only: [:index]
+  # resources :users do
+  	# resources :profiles, only: [:index]
+  # end
 end
