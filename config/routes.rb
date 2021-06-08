bce7872e0e375cb0b9442e20495a45d442b7a820
Kyle Danny <codingkyle92@gmail.com>
2021-06-08 16:15:12 +0200

    fixes to profile controller and user_interests controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+
   resource :user_interests, only: [ :new, :create ]
 
-  resources :profiles, only: [:index]
+  resources :profiles, only: [ :index ]
 
 end
