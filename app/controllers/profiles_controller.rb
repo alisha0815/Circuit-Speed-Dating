d51b3406d7eb385af0aee826680a24e1a4d6cd58
Kyle Danny <codingkyle92@gmail.com>
2021-06-09 08:49:07 +0200

    profile changes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,18 @@
 class ProfilesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
-  before_action :set_user, only: [:edit, :update]
+  before_action :set_user, only: [:update]
 
   def index
     @users = User.all
   end
 
+  def show
+    @user = current_user
+  end
+
   def edit
+    @user = current_user
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-
   resource :user_interests, only: [ :new, :create ]
 
-  resources :profiles, only: [ :index, :edit, :update ]
+  resource :profile, only: [ :show, :edit ]
+  resources :profiles, only: [ :index ]
 
+  resources :user, only: [ :update ]
 end
