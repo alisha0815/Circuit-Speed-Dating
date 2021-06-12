13f9998409dc7d491d4d2de4f33caca2d59aba67
jungah sohn <aliciasohn815@gmail.com>
2021-06-12 15:29:08 +0200

    booking validation added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,14 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  #validate :only_one_booking_event
-  #scope :active, where(:active => true)
+  validates :event, uniqueness: { scope: :user }
 
-  protected
-
-  #def only_one_booking_event
-  #  if active && Event.active.where("id != ?", id).exists?
-  #    errors.add(:name, "You've already booked this event!")
-  #  end  
-  #end
 end
