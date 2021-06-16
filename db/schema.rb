893bf492be88153fdfebe5c841543e11c3a41bdc
Kyle Danny <codingkyle92@gmail.com>
2021-06-16 16:04:10 +0200

    Neatening up code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_06_12_143017) do
 
   # These are extensions that must be enabled in order to support this database
@@ -44,6 +43,14 @@ ActiveRecord::Schema.define(version: 2021_06_12_143017) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["event_id"], name: "index_bookings_on_event_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "event_exceptions", force: :cascade do |t|
+    t.bigint "event_id", null: false
+    t.datetime "time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_event_exceptions_on_event_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -128,6 +135,7 @@ ActiveRecord::Schema.define(version: 2021_06_12_143017) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "events"
   add_foreign_key "bookings", "users"
+  add_foreign_key "event_exceptions", "events"
   add_foreign_key "likes", "users", column: "asker_id"
   add_foreign_key "likes", "users", column: "receiver_id"
   add_foreign_key "matches", "events"
